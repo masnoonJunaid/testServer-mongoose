6d949e396860ebb1065c037ef6a4cd744350cc69
Masnoon Junaid <masnoon20@gmail.com>
2020-02-26 17:34:40 +0530

    leaderId mongoose schema

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -42,5 +42,36 @@ leaderRouter.route('/')
 	.catch((err) => next(err));
 })
 
-
+leaderRouter.route('/:leaderId')
+.get((req,res, next) => {
+	Leaders.findById(req.params.leaderId)
+	.then((leader) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type','application/json');
+		res.json(leader);
+	},(err) => next())
+	.catch((err) => next(next));
+})
+.post((req,res,next) => {
+	res.statusCode = 403;
+	res.end('This operation is not supported on  /leaders/' + req.params.leaderId);
+})
+.put((req,res,next) => {
+	Leaders.findByIdAndUpdate(req.params.leaderId, {$set:req.body}, { new:  true})
+	.then((leader) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json(leader);
+	}, (err) => next(err))
+	.catch((err) => next())
+}).
+delete((req,res,next) => {
+	Leaders.findByIdAndRemove(req.params.leaderId)
+	.then((resp) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json(resp);
+	}, (err) => next(err))
+	.catch((err) => next(err));
+})
 module.exports =  leaderRouter;
